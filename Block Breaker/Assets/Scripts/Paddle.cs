988916cbba48752be477fa565155809e6179aeed
Мишустин Сергеевич <hugo@iMac-Misustin.local>
2019-04-24 23:19:23 +0300

    work with paddle

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWithInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    // Start is called beb  fore the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosition = Input.mousePosition.x / Screen.width * screenWithInUnits;
+        Vector2 paddlePosition = new Vector2(transform.position.x, 
+                                             transform.position.y);
+
+        paddlePosition.x = Mathf.Clamp(mousePosition, minX, maxX);
+        transform.position = paddlePosition;
+    } 
+}
