de514851953fec075fb397f6a1899947fbbd10aa
Hugo <hugo@Hugos-MacBook-Pro.local>
2019-05-09 00:14:51 +0300

    work with score label

--- Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -9,14 +9,14 @@ public class GameStatus : MonoBehaviour
     // config params
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
-    //[SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // state variables
     [SerializeField] int currentScore = 0;
 
     private void Start()
     {
-        //scoreText.text = currentScore.ToString();
+        scoreText.text = currentScore.ToString();
     }
 
     // Update is called once per frame
@@ -30,6 +30,7 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 
 }
