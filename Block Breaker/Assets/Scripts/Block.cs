3e50f6b38f8462e24a7ade733665b62ca8ae8e5e
Мишустин Сергеевич <hugo@Misustins-iMac.local>
2019-05-08 16:14:02 +0300

    work with block braker

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
 
-    private void OnCollisionExit2D(Collision2D collision)
+    [SerializeField] AudioClip audioClip;
+    private Level level;
+
+    private void Start()
     {
-        Destroy(gameObject);
+        level = FindObjectOfType<Level>();
+        level.AddBlock();
     }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<GameStatus>().AddToScore();
+        AudioSource.PlayClipAtPoint(audioClip, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.RemoveBlock();
+    }
 }
---
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int blockCount;
+    private SceneLoader sceneLoader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void AddBlock()
+    {
+        blockCount++;
+    }
+
+    public void RemoveBlock()
+    {
+        blockCount--;
+        if (blockCount <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+
+}
